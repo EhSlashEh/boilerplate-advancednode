6b506a90d1fa493bacc354595f4a3556bca77af2
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-03 14:10:24 +0000

    2

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index');
-
+  res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
 const PORT = process.env.PORT || 3000;
