ebbce048f6ff55f650271e2ffff041d63fa92df7
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-05 20:40:48 +0000

    Put in right spot

--- server.js
+++ server.js
@@ -43,10 +43,21 @@ myDB(async client => {
     res.render('profile', { username: req.user.username });
   })
 
-  app
-  .route('/profile')
+  app.route('/profile')
   .get(ensureAuthenticated, (req,res) => {
      res.render('profile');
+  });
+
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   passport.use(new LocalStrategy((username, password, done) => {
