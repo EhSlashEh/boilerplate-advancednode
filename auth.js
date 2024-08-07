1cd9b8d79dd1bd64583e1b77c96cea100da5707e
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-07 01:17:05 +0000

    fix password

--- auth.js
+++ auth.js
@@ -29,7 +29,6 @@ module.exports = function (app, myDataBase) {
   passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    // callbackURL: 'https://boilerplate-advancednode.sky020.repl.co/auth/github/callback'
     callbackURL: 'https://3000-freecodecam-boilerplate-ckhk2d0nfwt.ws-us115.gitpod.io/auth/github/callback'
   },
   function (accessToken, refreshToken, profile, cb) {
